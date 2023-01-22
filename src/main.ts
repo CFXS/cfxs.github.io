9b0494cb76c8457c985b2843ed0792fb0d87b544
RVeips <rihardsv@na.lv>
2023-01-22 21:58:44 +0200

    img load
    Signed-off-by: RVeips <rihardsv@na.lv>

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import "./Styles/style.css"
 import "./Styles/animations.css"
 import { MainLoadingScreen } from "./Components/MainLoadingScreen"
 
-new MainLoadingScreen(document.getElementById("main_loading_container"))
+var img = new Image()
+img.onload = () => {
+    new MainLoadingScreen(document.getElementById("main_loading_container"))
+}
+img.src = "res/bg.png"
